7d1c2de2817630e52cf6c3bcc1eaa76a5d3d3cce
Nnamocha Claret <devclareo@gmail.com>
2019-01-12 01:05:31 +0100

    update index.php

--- payment.php
+++ payment.php
@@ -32,25 +32,3 @@
 
 
 	echo 'Success! You have been charged N' . ($price);
-
-
-	PS dont forget the following:
-
-	0. modify the p_id paramters to suit your use case
-	1. your-publishable-key in index.html file (required)
-	2. url-to-prouct-image in index.html file (optional as the data-image attribute can be removed)
-	3. your-secret-key in payment.php (required)
-
-
-	4. download stripe-php-master api library folder from https://github.com/stripe/stripe-php
-
-	5. directory structure 
-		-index.html
-		-payment.php
-		-stripe-php-master (folder)
-
-
-	6. add the next lines to your css file
-	.stripe-button-el{
-          display: none !important;
-        }
